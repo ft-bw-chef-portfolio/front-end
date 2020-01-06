72bc872adda853d30e7124852c7f03e4418fc422
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-06 17:15:42 -0500

    adding style

--- login-page/src/components/Login.js
+++ login-page/src/components/Login.js
@@ -3,23 +3,29 @@ import React from 'react';
 function Login (){
     console.log();
     return(
-        <form>
-            <h1>Log in!</h1>
-           
-                <label htmlFor='email'>Email: </label>
-                <input 
-                id='email'
-                type='text'
-                placeholder='email'
-                name='email'
-                /> 
-                <label htmlFor='password'>Password: </label>
-                <input 
-                id='password'
-                type='text'
-                placeholder='password'
-                name='password'
-                />
+        <form className='form'>
+            <h1 className='login-header'>Log in!</h1>
+                <div className='form-content'>
+                   <div>
+                        <label className='email' htmlFor='email'>Email: </label>
+                    </div>
+                        <input 
+                        id='email'
+                        type='text'
+                        placeholder='email'
+                        name='email'
+                        />
+                        <div>
+                            <label className='password' htmlFor='password'>Password: </label>
+                        </div>
+                        <input 
+                        id='password'
+                        type='text'
+                        placeholder='password'
+                        name='password'
+                        />
+         
+                </div>
                 <button type='submit'>Submit</button>
                 <p>Don't have an account? Sign-up here!</p>
         </form>

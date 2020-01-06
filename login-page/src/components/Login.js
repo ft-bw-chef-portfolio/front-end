91ab48074edff1de604c2d593ac0ab297e6a5cf7
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-06 15:55:40 -0500

    finished adding content

--- login-page/src/components/Login.js
+++ login-page/src/components/Login.js
@@ -14,7 +14,14 @@ function Login (){
                 name='email'
                 /> 
                 <label htmlFor='password'>Password: </label>
-                
+                <input 
+                id='password'
+                type='text'
+                placeholder='password'
+                name='password'
+                />
+                <button type='submit'>Submit</button>
+                <p>Don't have an account? Sign-up here!</p>
         </form>
     )
 }

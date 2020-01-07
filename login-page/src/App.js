a2c1e19062423998a38a5c5063e1c08917093474
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-07 00:41:15 -0500

    finished final styling on login page

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './tea-cup.png';
 import './App.scss';
 import Login from './components/Login';
 import { Route } from "react-router-dom";
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header" />
         <img src={logo} className="App-logo" alt="logo" /> */}
-        <Login />
+        <header className='logo-header'>
+          <img src={logo} className='logo' alt='logo' />
+          <p>Chef Portfolio</p>
+        
 
         <main>
-          <Route exact path="/login">
+        <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/login">
             <Login />
           </Route>
+          
         </main>
+        </header>
     </div>
   );
 };
--- login-page/src/components/Login.js
+++ login-page/src/components/Login.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function Login (){
-    console.log();
+const Login = props => {
+    console.log(props)
+    const [info, setInfo] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChanges = e =>{
+        setInfo({
+            ...info,
+            [e.target.name]: e.target.value
+        });
+        console.log(e.target)
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        console.log(info)
+    }
+
+    // function myfunction() {
+    //     var x = document.getElementById('password');
+    //     if (x.type === 'password'){
+    //         x.type = 'text';
+    //     } else {
+    //         x.type = 'password';
+    //     }
+    // }
     return(
-        <form className='form'>
-            <h1 className='login-header'>Log in!</h1>
+        <form className='form' onSubmit={submitForm}>
+            <h2 className='login-header'>Log in!</h2>
                 <div className='form-content'>
-                   <div>
+                   <div className='email-content'>
                         <label className='email' htmlFor='email'>Email: </label>
                     </div>
                         <input 
@@ -14,20 +41,26 @@ function Login (){
                         type='text'
                         placeholder='email'
                         name='email'
+                        onChange={handleChanges}
                         />
-                        <div>
+                        <div className='password-content'>
                             <label className='password' htmlFor='password'>Password: </label>
                         </div>
                         <input 
                         id='password'
-                        type='text'
-                        placeholder='password'
+                        type='password'
+                        placeholder='Enter password'
                         name='password'
+                        onChange={handleChanges}
                         />
-         
+                        {/* <input
+                        id='checkbox' 
+                        type='checkbox' 
+                        value=''
+                        onClick='myfunction()' />Show Password */}
                 </div>
                 <button type='submit'>Submit</button>
-                <p>Don't have an account? Sign-up here!</p>
+                <Link className='link-signup' to='/signup'>Don't have an account? Sign-up here!</Link>
         </form>
     )
 }

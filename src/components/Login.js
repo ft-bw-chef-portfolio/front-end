1a131f418d4d6b588de5389b54394a1f129ad3c6
kika <anamonteiro430@gmail.com>
2020-01-09 17:29:34 +0000

    final commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,76 +5,81 @@ import { loginUser } from '../store/actions';
 import { Redirect } from 'react-router';
 
 const Login = () => {
-    const dispatch = useDispatch();
-    //dispatch can be used to pull actions instead of connect
-    const {isLoggedin, isLogging} = useSelector((state) => state);
-    //useSelector can be used to pull state from reducers
+  const dispatch = useDispatch();
+  //dispatch can be used to pull actions instead of connect
+  const { isLoggedin, isLogging } = useSelector(state => state);
+  //useSelector can be used to pull state from reducers
 
-    const [info, setInfo] = useState({
-        username: "",
-        password: ""
+  const [info, setInfo] = useState({
+    username: '',
+    password: ''
+  });
+
+  if (isLogging) {
+    return <h2>Logging In...</h2>;
+  }
+
+  const handleChanges = e => {
+    setInfo({
+      ...info,
+      [e.target.name]: e.target.value
     });
+    console.log(e.target);
+  };
 
-    if (isLogging) {
-        return <h2>Logging In...</h2>;
-      }
+  const submitForm = e => {
+    e.preventDefault();
+    dispatch(loginUser(info));
+    console.log(info);
+  };
 
-    const handleChanges = e => {
-        setInfo({
-            ...info,
-            [e.target.name]: e.target.value
-        });
-        console.log(e.target)
-    }
+  if (isLoggedin) return <Redirect to="/" />;
 
-    const submitForm = e => {
-        e.preventDefault();
-        dispatch(loginUser(info))
-        console.log(info)
-    }
-
-if (isLoggedin) return <Redirect to="/"/>
-
-    // function myfunction() {
-    //     var x = document.getElementById('password');
-    //     if (x.type === 'password'){
-    //         x.type = 'text';
-    //     } else {
-    //         x.type = 'password';
-    //     }
-    // }
-    return (
-        <div className='login-container'>
-        <form className='form' onSubmit={submitForm}>
-            <h1 className='login-header'>Log in!</h1>
-            <div className='form-content'>
-                <div className='username-content'>
-                    <label className='username' htmlFor='username'>username: </label>
-                </div>
-                <input
-                    id='username'
-                    type='text'
-                    placeholder='Enter username'
-                    name='username'
-                    onChange={handleChanges}
-                />
-                <div className='password-content'>
-                    <label className='password' htmlFor='password'>Password: </label>
-                </div>
-                <input
-                    id='password'
-                    type='password'
-                    placeholder='Enter password'
-                    name='password'
-                    onChange={handleChanges}
-                />
-            
-            </div>
-            <button type='submit'>Submit</button>
-            <Link className='link-signup' to='/signup'>Don't have an account? Sign-up here!</Link>
-        </form>
+  // function myfunction() {
+  //     var x = document.getElementById('password');
+  //     if (x.type === 'password'){
+  //         x.type = 'text';
+  //     } else {
+  //         x.type = 'password';
+  //     }
+  // }
+  return (
+    <div className="login-container">
+      <form className="form" onSubmit={submitForm}>
+        <h1 className="login-header">Log in!</h1>
+        <div className="form-content">
+          <div className="username-content">
+            <label className="username" htmlFor="username">
+              username:{' '}
+            </label>
+          </div>
+          <input
+            id="username"
+            type="text"
+            placeholder="Enter username"
+            name="username"
+            onChange={handleChanges}
+          />
+          <div className="password-content">
+            <label className="password" htmlFor="password">
+              Password:{' '}
+            </label>
+          </div>
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            onChange={handleChanges}
+          />
         </div>
-    )
-}
+        <button type="submit">Submit</button>
+        <Link className="link-signup" to="/signup">
+          Don't have an account? Sign-up here!
+        </Link>
+      </form>
+    </div>
+  );
+};
 
-  export default Login;
+export default Login;

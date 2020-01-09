d547cbd2120465f1c3df1094797dc6263e5d0435
JimmyMcBride <mcbride967@gmail.com>
2020-01-09 16:04:52 -0600

    added new pieces of state to create a correctly formated object to send to back end

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -1,36 +1,55 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { createNewRecipe } from '../store/actions';
-import { Redirect } from 'react-router';
-import '../styles/styles.scss';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewRecipe } from "../store/actions";
+import { Redirect } from "react-router";
+import "../styles/styles.scss";
 
 const NewRecipe = () => {
   const dispatch = useDispatch();
   const { isPosting, isPosted } = useSelector(state => state);
   //state
-  const [recipe, setRecipe] = useState({
-    title: '',
-    image: '',
-    meal_type_id: '',
-    ingredients: [],
-    instructions: []
+
+  const [ingredients, setIngredients] = useState({});
+
+  const [instructions, setInstructions] = useState({
+    position: 1
   });
 
-  if (isPosting) {
-    return <h2>Posting Recipe</h2>;
-  }
+  const [recipe, setRecipe] = useState({
+    title: "",
+    image: "",
+    meal_type_id: "",
+    ingredients: ingredients,
+    instructions: instructions
+  });
+
+  if (isPosting) return <h2>Posting Recipe</h2>;
 
   const handleChanges = e => {
     setRecipe({
       ...recipe,
       [e.target.name]: e.target.value
     });
-    console.log(e.target);
+    // console.log(e.target);
+  };
+
+  const handleIngredients = e => {
+    setIngredients({
+      ...ingredients,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleInstructions = e => {
+    setInstructions({
+      ...instructions,
+      [e.target.name]: e.target.value
+    });
   };
 
   const submitForm = e => {
     e.preventDefault();
-    dispatch(createNewRecipe(recipe));
+    dispatch(createNewRecipe(recipe, ingredients, instructions));
     console.log(recipe);
   };
 
@@ -63,18 +82,18 @@ const NewRecipe = () => {
             onChange={handleChanges}
           />
           <input
-            name="ingredients"
+            name="name"
             className="ingredients"
             placeholder="Ingredients"
             type="text"
-            onChange={handleChanges}
+            onChange={handleIngredients}
           />
           <textarea
-            name="instructions"
+            name="description"
             className="instructions"
             placeholder="Instructions"
             type="text"
-            onChange={handleChanges}
+            onChange={handleInstructions}
           />
           <button type="submit">Submit New Recipe</button>
         </form>

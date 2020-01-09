cde8b170924ee95d3522cf9a5a263e70c8a04d5d
kika <anamonteiro430@gmail.com>
2020-01-09 16:59:23 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Recipe from './components/Solo_Recipe';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import NewRecipe from './components/NewRecipe';
+import EditRecipe from './components/EditRecipe';
 import Portfolio from './components/Portfolio';
 import ChefUser from './components/ChefUser';
 
@@ -23,6 +24,7 @@ function App() {
       <Route path="/newrecipe" component={NewRecipe} />
       <Route path="/chefs/:id/recipes" component={Portfolio} />
       <Route path="/chefUser" component={ChefUser} />
+      <Route path="/editrecipe" component={EditRecipe} />
     </div>
   );
 }
--- src/components/EditRecipe.js
+++ src/components/EditRecipe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import '../styles/_signup.scss';
+import '../styles/styles.scss';
 
 const EditRecipe = () => {
   const { handleSubmit, register, errors, watch } = useForm();

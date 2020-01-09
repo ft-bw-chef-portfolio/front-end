307a6b0aa50fcf9f55853bb468cfc96c8e7df9b4
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-09 16:21:45 -0500

    finished styling the recipe card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -8,7 +8,7 @@ function RecipeCard({ recipe, error }) {
       <Link to={`/${recipe.id}`}> 
         <div className="single-recipe">
           {error && <p>{error}</p>}
-          <h1>{recipe.title}</h1>
+          <h1 className="recipe-title">{recipe.title}</h1>
           <p>{console.log(recipe.chef)}</p>
           <img src={recipe.image} alt="recipe pic" width="200px" height="200px" />
           {/* <h3>Chef: {recipe.chef && recipe.chef.name}</h3> We'll add this when Backend updates */}  

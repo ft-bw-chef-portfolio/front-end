925b2ac1ecf35a571f4ff56bffa95612e05d469d
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-08 21:11:44 -0500

    styled nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import '../images/userimg.png';
+import '../styles/styles.scss';
 
 function Nav(){
 
     return(
-        <div>
+        <div className='nav'>
             <Link to="/">Home</Link>
             {/* {!isLoggedin} */}
+            {/* <div>
+            <img className='userimg' src='src\images\userimg.png' alt='user' />
+            </div> */}
             <Link to="/login" >Chef Login</Link>
+            {/* <div>
+            <img className='loginimg' src='src\images\loginimg.png' alt='user' />
+            </div> */}
             <Link to="/signup">Chef Signup</Link>
             <Link to >Signout</Link>
             <Link to >Add Recipe</Link>

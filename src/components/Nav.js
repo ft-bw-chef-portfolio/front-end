543037447476d5878a5202ebc3470f2717e81853
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-09 20:09:00 -0500

    finished nav and fix login page styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import '../styles/_nav.scss';
+import Homeicon from '../images/homeicon.png';
+import Loginicon from '../images/userimg.png';
+import Signupicon from '../images/signimg.png';
+import Logouticon from '../images/logout.png';
+import Addicon from '../images/plus.png';
 
 function Nav(){
 
     return(
         <div className='nav'>
-            <Link to="/"><img className='homeimg' src='src\images\homeicon.png' alt='home' />Home</Link>
+            <Link to="/"><img src={Homeicon} alt='home' />Home</Link>
             {/* {!isLoggedin} */}
             <div>
             
             </div>
-            <Link to="/login" ><img className='userimg' src='src\images\userimg.png' alt='login' />Chef Login</Link>
+            <Link to="/login" ><img className='userimg' src={Loginicon} alt='login' />Chef Login</Link>
             <div>
             </div>
-            <Link to="/signup"><img className='loginimg' src='src\images\loginimg.png' alt='signup' />
+            <Link to="/signup"><img className='loginimg' src={Signupicon} alt='signup' />
 Chef Signup</Link>
-            <Link to ><img className='logout' src='src\images\logout.png' alt='logout' />Signout</Link>
-            <Link to ><img className='addrecipe' src='src\images\add.png' alt='add recipe' />Add Recipe</Link>
+            <Link to ><img className='logout' src={Logouticon} alt='logout' />Signout</Link>
+            <Link to ><img className='addrecipe' src={Addicon} alt='add recipe' />Add Recipe</Link>
         </div>
     )
 }
